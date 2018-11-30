c6496df4699f890f1a6170a8b27f9ac2790b151e
siebedc2 <34480969+siebedc2@users.noreply.github.com>
2018-11-30 14:59:20 +0100

    Layout aanmaken
    Ik heb de layout gemaakt en dan ook de texvelden opgehaald in MainActivity.java

--- app/src/main/java/com/example/android/pietjesbak/MainActivity.java
+++ app/src/main/java/com/example/android/pietjesbak/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.pietjesbak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //DECLARATIE
+        //Speler 1
+        TextView naamSpeler1 = (TextView) findViewById(R.id.naamSpeler1);
+        TextView puntenSpeler1 = (TextView) findViewById(R.id.puntenSpeler1);
+        TextView beurtSpeler1 = (TextView) findViewById(R.id.beurtSpeler1);
+        TextView worpSpeler1 = (TextView) findViewById(R.id.worpSpeler1);
+
+        //Speler 2
+        TextView naamSpeler2 = (TextView) findViewById(R.id.naamSpeler2);
+        TextView puntenSpeler2 = (TextView) findViewById(R.id.puntenSpeler2);
+        TextView beurtSpeler2 = (TextView) findViewById(R.id.beurtSpeler2);
+        TextView worpSpeler2 = (TextView) findViewById(R.id.worpSpeler2);
+
+        //Buttons
+        Button buttonPass = (Button) findViewById(R.id.button_pass);
+        Button buttonWerp = (Button) findViewById(R.id.button_werp);
+
+        //Dobbelstenen
+        CheckBox dobbelsteen1 = (CheckBox) findViewById(R.id.dobbelsteen1);
+        CheckBox dobbelsteen2 = (CheckBox) findViewById(R.id.dobbelsteen2);
+        CheckBox dobbelsteen3 = (CheckBox) findViewById(R.id.dobbelsteen3);
+
     }
 }
