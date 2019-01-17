e3d1787ec842a72a8337d48d1b85f78f3bace2bd
siebedc2 <siebedc2@gmail.com>
2019-01-17 19:20:09 +0100

    Second commit

--- app/src/main/java/com/example/android/pietjesbak/MainActivity.java
+++ app/src/main/java/com/example/android/pietjesbak/MainActivity.java
@@ -2,38 +2,69 @@ package com.example.android.pietjesbak;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity { 
+import static com.example.android.pietjesbak.R.id.puntenSpeler2;
+
+public class MainActivity extends AppCompatActivity {
+    //DECLARATIE
+    //Speler 1
+    TextView naamSpeler1, puntenSpeler1, beurtSpeler1, worpSpeler1;
+
+    //Speler 2
+    TextView naamSpeler2, puntenSpeler2, beurtSpeler2, worpSpeler2;
+
+    //Buttons
+    Button pas, werp;
+
+    //Dobbelstenen
+    CheckBox dobbelsteen1, dobbelsteen2, dobbelsteen3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //DECLARATIE
         //Speler 1
-        TextView naamSpeler1 = (TextView) findViewById(R.id.naamSpeler1);
-        TextView puntenSpeler1 = (TextView) findViewById(R.id.puntenSpeler1);
-        TextView beurtSpeler1 = (TextView) findViewById(R.id.beurtSpeler1);
-        TextView worpSpeler1 = (TextView) findViewById(R.id.worpSpeler1);
+        naamSpeler1 = (TextView) findViewById(R.id.naamSpeler1);
+        puntenSpeler1 = (TextView) findViewById(R.id.puntenSpeler1);
+        beurtSpeler1 = (TextView) findViewById(R.id.beurtSpeler1);
+        worpSpeler1 = (TextView) findViewById(R.id.worpSpeler1);
 
         //Speler 2
-        TextView naamSpeler2 = (TextView) findViewById(R.id.naamSpeler2);
-        TextView puntenSpeler2 = (TextView) findViewById(R.id.puntenSpeler2);
-        TextView beurtSpeler2 = (TextView) findViewById(R.id.beurtSpeler2);
-        TextView worpSpeler2 = (TextView) findViewById(R.id.worpSpeler2);
+        naamSpeler2 = (TextView) findViewById(R.id.naamSpeler2);
+        puntenSpeler2 = (TextView) findViewById(R.id.puntenSpeler2);
+        beurtSpeler2 = (TextView) findViewById(R.id.beurtSpeler2);
+        worpSpeler2 = (TextView) findViewById(R.id.worpSpeler2);
 
         //Buttons
-        Button buttonPass = (Button) findViewById(R.id.button_pass);
-        Button buttonWerp = (Button) findViewById(R.id.button_werp);
+        pas = (Button) findViewById(R.id.button_pass);
+        werp = (Button) findViewById(R.id.button_werp);
 
         //Dobbelstenen
-        CheckBox dobbelsteen1 = (CheckBox) findViewById(R.id.dobbelsteen1);
-        CheckBox dobbelsteen2 = (CheckBox) findViewById(R.id.dobbelsteen2);
-        CheckBox dobbelsteen3 = (CheckBox) findViewById(R.id.dobbelsteen3);
+        dobbelsteen1 = (CheckBox) findViewById(R.id.dobbelsteen1);
+        dobbelsteen2 = (CheckBox) findViewById(R.id.dobbelsteen2);
+        dobbelsteen3 = (CheckBox) findViewById(R.id.dobbelsteen3);
+
+        //Indien je op 'werp' klikt gebeurt dit
+        werp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Indien je op 'pass' klikt gebeurt dit
+        pas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
 }
