fba8f09cb5e223aaec57e109a54d6ff9455ed736
robinvanbuggenhout <robinvanbuggenhout@icloud.com>
2019-01-18 17:51:21 +0100

    update

--- app/src/main/java/com/example/android/pietjesbak/MainActivity.java
+++ app/src/main/java/com/example/android/pietjesbak/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.pietjesbak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
     //DECLARATIE
     //Speler 1
     TextView naamSpeler1, puntenSpeler1, beurtSpeler1, worpSpeler1, worp2Speler1, worp3Speler1;
@@ -21,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
     TextView naamSpeler2, puntenSpeler2, beurtSpeler2, worpSpeler2, worp2Speler2, worp3Speler2;
 
     //Buttons
-    Button pas, werp;
+    Button btn, pas, werp;
+
+    // EditText
+    EditText et;
+
+    // String
+    String st;
 
     //Dobbelstenen
     CheckBox dobbelsteen1, dobbelsteen2, dobbelsteen3;
@@ -33,6 +42,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+
+        et = findViewById(R.id.editext);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this.SecondActivity.class);
+                    st = et.getText().toString();
+                    i.putExtra("value",st);
+                    startActivity(i);
+                    finish();
+            }
+        });
+
+
 
         //Speler 1
         // naamSpeler1 = (TextView) findViewById(R.id.naamSpeler1);
@@ -78,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    int ogen = 1;
+    // int ogen = 1;
 
     //Indien je op 'werp' klikt gebeurt dit
     public void generate(View view) {
